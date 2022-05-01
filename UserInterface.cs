c951ab45538ddfb1ea4f10f4092dc6d2a74518c0
me-ve <68286584+me-ve@users.noreply.github.com>
2022-05-01 22:13:57 +0200

    change the way of executing commands

--- UserInterface.cs
+++ UserInterface.cs
@@ -5,35 +5,34 @@ namespace interspace
 {
 	static class UserInterface{
 		// there are stored the commands for interacting with the user
+		delegate ErrorCode Command();
 		static Dictionary<ErrorCode, string> errorCommunicates = new Dictionary<ErrorCode, string>(){
 			//sent to console during error
 			{ErrorCode.NO_ERROR, ""},
-			{ErrorCode.FILE_GENERAL_ERROR, "There was a problem with loading file"},
-			{ErrorCode.FILE_LOADING_ERROR, "The file does not exist"},
-			{ErrorCode.MATRIX_NULL_ERROR, "The matrix is not initialized"},
-			{ErrorCode.MATRIX_SIZE_ERROR, "The matrix size must be positive"},
-			{ErrorCode.MATRIX_FORMAT_ERROR, "The matrix provided in input has wrong format"},
-			{ErrorCode.MATRIX_INDEX_ERROR, "The index was outside of the bounds of matrix"},
-			{ErrorCode.UNKNOWN_COMMAND_ERROR, "Unknown command"},
+			{ErrorCode.FILE_GENERAL_ERROR,		"There was a problem with loading file"},
+			{ErrorCode.FILE_LOADING_ERROR,		"The file does not exist"},
+			{ErrorCode.MATRIX_NULL_ERROR,		"The matrix is not initialized"},
+			{ErrorCode.MATRIX_SIZE_ERROR,		"The matrix size must be positive"},
+			{ErrorCode.MATRIX_FORMAT_ERROR,		"The matrix provided in input has wrong format"},
+			{ErrorCode.MATRIX_INDEX_ERROR,		"The index was outside of the bounds of matrix"},
+			{ErrorCode.UNKNOWN_COMMAND_ERROR,	"Unknown command"},
 		};
-		static ErrorCode DoAction(string[] command){
-			string[] args = command.Where((item, index) => index != 0).ToArray();	//get other arguments
+		static Dictionary<string, Command> commands = new Dictionary<string, Command>(){
+			{"create",	Commands.CreateMatrixFromStdin},
+			{"draw",	Commands.DrawMatrix},
+			{"help",	Commands.Help},
+			{"history",	Commands.DisplayHistory},
+			{"load",	Commands.LoadMatrixFromFile},
+			{"exit",	Commands.CloseProgram}
+		};
+		static ErrorCode DoAction(string[] commandParams){
+			string[] args = commandParams.Where((item, index) => index != 0).ToArray();	//get other arguments
 			//TODO process those arguments too
-			switch(command[0]){
-				case "create":
-				return Commands.CreateMatrixFromStdin();
-				case "draw":
-				return Commands.DrawMatrix();
-				case "help":
-				return Commands.Help();
-				case "history":
-				return Commands.DisplayHistory();
-				case "load":
-				return Commands.LoadMatrixFromFile();
-				case "exit":
-				return Commands.CloseProgram();
-			}
-			return ErrorCode.UNKNOWN_COMMAND_ERROR;
+			Command command;
+			if(!commands.TryGetValue(commandParams[0], out command))
+				return ErrorCode.UNKNOWN_COMMAND_ERROR;
+			else
+				return command.Invoke();
 		}
 		static string[] GetCommand(){
 			string lineBeginning = $"{ApplicationData.linesExecuted}> ";
